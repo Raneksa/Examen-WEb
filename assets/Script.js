537fc83d03927f34b53318c4d06e739dbe4c0c2d
Metosela <hei.metosela@gmail.com>
2025-04-19 09:34:32 +0200

    script

--- assets/Script.js
+++ assets/Script.js
@@ -1,1 +1,29 @@
 nekena
+const paragraph = [
+    "apple", "banana", "grape", "orange", "cherry"
+];
+
+const typingText = document.querySelector(".typing-text p")
+const inField = document.querySelector(".wrapper .input-field")
+const tryAgainBtn = document.querySelector(".content button")
+const timeTag = document.querySelector(".time span b")
+const mistakeTag = document.querySelector(".mistake span")
+const wpmTag = document.querySelector(".wpn span")
+const cpmTag = document.querySelector(".cpm span")
+
+let timer;
+let maxTime = 60;
+let timeleft = maxTime;
+let charIndex = mistakes = isTyping = 0;
+
+function loadParagraph(){;
+    const ranIndex = Math.floor(Math.random() * paragraph.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span>`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelector("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inField.focus());
+    typingText.addEventListener("click", () => inField.focus());
+}
