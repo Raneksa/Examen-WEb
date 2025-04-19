fff19742c1060db6d5a35f1108ebef8a3066df5f
Metosela <hei.metosela@gmail.com>
2025-04-19 12:57:20 +0200

    Update Script.js

--- assets/Script.js
+++ assets/Script.js
@@ -27,3 +27,70 @@ function loadParagraph(){;
     document.addEventListener("keydown", () => inField.focus());
     typingText.addEventListener("click", () => inField.focus());
 }
+
+function initTyping(){
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeleft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if (typedChar == null) {
+            if (charIndex > 0) {
+                charIndex--;
+                if(characters[charIndex].classList.contains("incorrect")){
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        }else{
+            if (characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            }else{
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+        let wpn = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeleft) * 60);
+        wpn = wpn < 0 || !wpn || wpn === Infinity ? 0: wpn;
+
+        wpmTag.innerText = wpn;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes
+    }else{
+        clearInterval(timer);
+        inField.value = "";
+    }
+}
+
+function initTimer(){
+    if(timeleft > 0){
+        timeleft--;
+        timeTag.innerText = timeleft;
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeleft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeleft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inField.value = "";
+    timeTag.innerText = timeleft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame)
